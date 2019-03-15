bb4245a8631f2a3dfb98d52483b7c215692593d8
Yogesh Sawant <yogeshswnt27@gmail.com>
2019-03-14 22:21:05 -0400

    Final Commit
    Done

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_4hQmo5ptC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1o8vg14pag7rur7t5qeqcvijae', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://j08bg04ivf.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
